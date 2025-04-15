483070a31bcd0dcd2cf72053332cff75611eca4e
JCGreen03 <160683432+JCGreen03@users.noreply.github.com>
2025-04-15 16:01:20 -0400

    Add files via upload

---
+++ Arrays/ArrayHome.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update Employee</title>
+    <link rel="stylesheet" type="text/css" href="./CSS/Variables.css">
+</head>
+
+<body>
+
+<?php include ("./Components/variables.php"); ?>
+
+<?php include ("./Components/EmployeeArray.php"); ?>
+
+<?php include ("./Components/Navbar.php"); ?>
+
+<?php include ("./Components/EmployeeControl.php"); ?>
+
+<?php include ("./Components/Footer.php"); ?>
+
+</body>
