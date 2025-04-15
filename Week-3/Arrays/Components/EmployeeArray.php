af581fbbd2fc9cefa844c0bc7b53cde4f169a347
JCGreen03 <160683432+JCGreen03@users.noreply.github.com>
2025-04-15 16:01:46 -0400

    Add files via upload

---
+++ Week-3/Arrays/Components/EmployeeArray.php
@@ -0,0 +1,7 @@
+
+<?php
+$employeeName = array($vincent->employee_name, $guy->employee_name, $reggie->employee_name);
+$employeeRank = array($vincent->employee_rank, $guy->employee_rank, $reggie->employee_rank);
+$employeeEmail = array($vincent->employee_email, $guy->employee_email, $reggie->employee_email);
+$employeeFavoriteBook = array($vincent->employee_favoriteBook, $guy->employee_favoriteBook, $reggie->employee_favoriteBook);
+?>
